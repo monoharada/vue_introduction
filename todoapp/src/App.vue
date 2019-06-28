f6ea14310941f5287293dc081f7b76923a78d999
monoharada <harada@monosus.co.jp>
2019-06-28 17:46:06 +0900

    App.vue_fix

--- todoapp/src/App.vue
+++ todoapp/src/App.vue
@@ -1,16 +1,40 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+  div
+    h3 My Todo
+    input(v-model="newTodo" placeholder="Input here...")
+    button(@click="addTodo()") Add
+
+    h5 Todo List
+    ul
+      li(v-for="(todo,i) in todos" v-bind:key="i")
+        | {{todo}}
+        button(v-on:click="deleteTodo(i)") Del
 </template>
+<script>
+export default {
+  data() {
+    return {
+      todos: ["hoge", "fuga", "boo"],
+      newTodo: ""
+    };
+  },
+  methods: {
+    addTodo() {
+      // if (this.newTodo === "") return;
+      this.todos.push(this.newTodo);
+      this.newTodo = "";
+    },
+    deleteTodo(i) {
+      this.todos.splice(i, 1);
+    }
+  }
+};
+</script>
+
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
